2b0bfb01383368d7afad6205fbfb6f882f290d38
Matthew Fatheree <Matthew.Fatheree@belkin.com>
2014-06-11 09:01:58 -0700

    add find and replace text function

---
+++ mfstdsrc/find_and_replace_txt.c
@@ -0,0 +1,52 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+#include <errno.h>
+#include <fcntl.h>
+
+
+#define BUF_SIZE 1024
+#define PKT_SIZE ( BUF_SIZE * 4 )
+
+
+
+int find_and_replace_text( char *sbuf, int buflen, char *from, char *to )
+{
+	char *buf1;
+	char *startbuf;
+	char *cstart;
+	int offs = 0;
+	int roffs = 0;
+	int len = 0;
+	
+	startbuf = sbuf;
+	
+	buf1 = malloc( sizeof(char) * buflen );
+	if( buf1 == NULL)return -1;
+	
+	memset(buf1, 0, buflen);
+	
+	cstart = strstr(startbuf, from);
+	while( cstart != NULL ) {
+		len = cstart - startbuf;
+		roffs = cstart - sbuf;
+		printf("\n\rfound %s @ %d\n\r", from, roffs);
+		memcpy(buf1 + offs, startbuf, len);
+		offs += len;
+		memcpy(buf1 + offs, to, strlen(to));
+		offs += strlen(to);
+		roffs += strlen(from);
+		startbuf = sbuf + roffs;
+		cstart = strstr(startbuf, from);
+	}
+	if(( offs + strlen(startbuf)) > buflen ){
+		printf("error - replace would overflow\n\r");
+	} else {
+		//copy the rest of the buffer
+		memcpy(buf1 + offs, sbuf + roffs, strlen(sbuf + roffs));
+	}
+	memset(sbuf, 0, buflen);
+	memcpy(sbuf, buf1, strlen(buf1));
+	free(buf1);
+	return 0;
+}
