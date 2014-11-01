7d81bf021805995b42aa64821d769988e7b61bb7
Matthew Fatheree <Matthew.Fatheree@belkin.com>
2014-10-31 17:16:06 -0700

    some useful c macros I use

---
+++ mfstdsrc/C_macros.c
@@ -0,0 +1,41 @@
+#define SET_BIT(p,n)      ((p) |= (1 << (n)))
+#define CLR_BIT(p,n)      ((p) &= (~(1) << (n)))
+#define IS_BIT_SET(val, bit_no) (((val) >> (bit_no)) & 1)
+
+#define IMPLIES(x, y) (!(x) || (y))
+
+#define COMPARE(x, y) (((x) > (y)) - ((x) < (y)))
+#define SIGN(x) COMPARE(x, 0)
+
+#define SWAP(x, y, T) do { T tmp = (x); (x) = (y); (y) = tmp; } while(0)
+#define SORT2(a, b, T) do { if ((a) > (b)) SWAP((a), (b), T); } while (0)
+
+#define BZERO(x, y)		memset(x, 0, y)
+
+
+#if defined NDEBUG
+    #define TRACE( format, ... )
+#else
+    #define TRACE( format, ... )   printf( "%s::%s(%d)" format, __FILE__, __FUNCTION__,  __LINE__, __VA_ARGS__ )
+#endif
+
+#define LENGTH(array) (sizeof(array) / sizeof (array[0]))
+
+#define QUOTE(name) #name
+#define STR(name) QUOTE(name)
+
+#define Loop(i,x) for(i=0; i<x; i++)
+
+/// Determine whether the given signed or unsigned integer is odd.
+#define IS_ODD( num )   ((num) & 1)
+ 
+/// Determine whether the given signed or unsigned integer is even.
+#define IS_EVEN( num )  (!IS_ODD( (num) ))
+ 
+/**
+Determine whether the given number is between the other two numbers
+(both inclusive).
+*/
+#define IS_BETWEEN( numToTest, numLow, numHigh )
+        ((unsigned char)((numToTest) >= (numLow) && (numToTest) <= (numHigh)))
+				
