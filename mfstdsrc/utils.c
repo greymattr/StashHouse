cf7894e4eca43bd930426fa93553ac135c332757
Matthew Fatheree <matthewf@NAIRV-024649>
2017-04-06 09:44:59 -0700

    update to add more utility functions

--- mfstdsrc/utils.c
+++ mfstdsrc/utils.c
@@ -16,7 +16,18 @@
  **************************************************************/
  
 #include "mfutils.h" 
- 
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <sysexits.h>
+#include <unistd.h>
+#include <pty.h>
+#include <fcntl.h>
+#include <stdarg.h>
+#include <stdbool.h>
+#include <ctype.h>
+
  
 int read_word(int fd, char *buf, int maxlen)
 {
@@ -49,5 +60,180 @@ int read_word(int fd, char *buf, int maxlen)
     }
     return i;
 }
+
+/* verifys a mac address array */
+int verify_addr(char *s) {
+	int i;
+	for(i = 0; i < 17; i++) {
+		if(i % 3 != 2 && !isxdigit(s[i]))
+			return 0;
+		if(i % 3 == 2 && s[i] != ':')
+			return 0;
+	}
+	if(s[17] != '\0')
+		return 0;
+	return 1;
+}
+
+/* like fprintf but using a file descriptor */
+int fdprintf( int fd, const char *fmt, ... )
+{
+  char buf[512];
+  va_list args;
+  if ( fd < 0 ) {
+    return -1;
+  }
+  va_start( args, fmt );
+  vsprintf( buf, fmt, args );
+  va_end( args );
+  return ( write( fd, buf, strlen( buf ) ) );
+}
+
+/* fdreadline - is used to read a line from a file descriptor
+ * into buf, of max length len.  buf will be memset to 0 for
+ * length len before use. fdrealine returns the length of the
+ * data it read from the fd
+ */
+int fdreadline( int fd, char *buf, int len )
+{
+  int i=0;
+  char ch;
+  int ok;
+  memset( buf, 0, len );
+  while( i < len ) {
+    if ( ( ok = read( fd, &ch, 1 ) ) == 1 ) {
+      if( ( ch != '\n' ) && ( ch != '\r' ) ) {
+        buf[i] = ch;
+        i++;
+      } else {
+        return i;
+      }
+    } else {
+      return ok;
+    }
+  }
+  return i;
+}
+
+/* fdreadline_t - is like fdreadline but uses select as a timeout
+ */
+int fdreadline_t( int fd, char *buf, int len, int maxtime )
+{
+	fd_set set;
+  struct timeval timeout;
+  int rv;
+	int ok = 0;
+	int rlen = 0;
+
+	while(1) {
+		FD_ZERO(&set); 
+		FD_SET(fd, &set); 
+
+		timeout.tv_sec = maxtime;
+		timeout.tv_usec = 0;
+
+		rv = select(fd + 1, &set, NULL, NULL, &timeout);
+		if(rv == -1) {
+			return rv;
+		} else if ( rv == 0 ) {
+			break;
+		} else {
+			ok = read( fd, buf+rlen, len-rlen );
+			if( ok > 0 )rlen += ok;
+			if( rlen == len )break;
+		}	
+	}
+	return rlen;
+}
+
+/* search_string - searches for string key, in buf
+ * if string key exists in buf, it will return the
+ * position in buf where string key starts.  else
+ * returns 0;
+ */
+int search_string( char *key, char *buf )
+{
+	int ok = -1;
+  char *tmp;
+  if( ( tmp = strstr( buf, key ) ) != NULL ) {
+    ok = ( tmp - buf );
+  }
+  return ok;
+}
+
+/* suspect - is kind of like expect, but less useful most of the time */
+int suspect( int fd, char *key, int timeout )
+{
+  int timer = 0;
+  char buf[2048];
+  int ok;
+  int len = 0;
+
+  memset( buf, 0, sizeof( buf ) );
+  fcntl( fd, F_SETFL, O_NONBLOCK );
+  while ( 1 ) {
+    ok = read ( fd, buf+len, sizeof( buf )-len );
+    if( ok > 0 ) {
+      if( ( len + ok ) < sizeof( buf ) ) {
+        len += ok;
+      } else {
+        ok = SUSPECT_ERR_MAX_READ;
+        break;
+      }
+      if( ( ok = search_string( key, buf ) ) >= 0 ) {
+        break;
+      }
+    }
+    sleep( 1 );
+    timer++;
+    //printf("timer = %d\n", timer);
+    if( timer == timeout ) {
+      ok = SUSPECT_ERR_TIMEOUT;
+      break;
+    }
+  }
+  //printf("\n\r->%s<-\n\r", buf);
+  return ok;
+}
+
+
+/* I wanted a call like 'popen' that would allow for simultanous
+ * reading and writting.  but much to my surprize, one does not exist,
+ * and pipes by their nature are uni directional, so I searched around and
+ * came up with this forkpty solution.  forkpty will create a parent
+ * and a child process in a psuedo tty and assign a freakin' file descriptor
+ * ( not file pointer ) to that pty. from there you can use system, or
+ * an execp call from the parent process to start up another program
+ * and then interact with it via read/writes to the file descriptor.
+ * HAPPY DAY!. Although a convoluted solution.
+ */
+int fdopenexec( char *executable )
+{
+  int ok = 0;
+  int master;
+  struct winsize win = { /* this structure is needed for forkpty */
+    .ws_col = 80, .ws_row = 24,
+    .ws_xpixel = 480, .ws_ypixel = 192,
+  };
+  pid_t child;
+  child = forkpty( &master, NULL, NULL, &win );
+  if ( child == -1 ) {
+    perror( "forkpty failed" );
+    return 0;
+  }
+  if ( child == 0 ) {
+    /* start the executable that we need to control from the parent process */
+    system( executable );
+    exit( EX_OSERR );
+  } else {
+    /* this is the child process, attached to a psuedo tty,
+     * running the exernal executable, which should allow low-level
+     * read/write calls - YEA LINUX! ( fread and fwrite are for newbs )
+		 */
+    ok = dup( master );
+  }
+  return ok;
+}
+
  
  
