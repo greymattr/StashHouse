cfbb4323ebbc670d3fbce723ee5c0c0243531c26
Matthew Fatheree <matthewfatheree@gmail.com>
2020-09-02 12:21:15 -0700

    update to code to use DEVICE_HAS defines, and add temp humidity and SD1306 screen functionality

--- ESP8266_archive/ESP12E_Device_template/src/main.cpp
+++ ESP8266_archive/ESP12E_Device_template/src/main.cpp
@@ -23,9 +23,43 @@
  * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
  * SOFTWARE.
  */
+ /*  Include all headers needed for full device functionality */
+ #include <Arduino.h>
+ #include <ESP8266WiFi.h>          // https://github.com/esp8266/Arduino
+ #include <DNSServer.h>            // Include for network functionality
+ #include <ESP8266WebServer.h>     // Include for web server functionality
+ #include <WiFiManager.h>          // https://github.com/tzapu/WiFiManager
+ #include <WiFiClient.h>           // for connection to AP
+ #include <Ticker.h>               // for LED status indicator
+ #include <NTPClient.h>
+ #include <WiFiUdp.h>
+ #include <ArduinoOTA.h>
+ #include <ESP8266mDNS.h>          // Include the mDNS library
+ #include <EEPROM.h>               // for storing configuration
+ #include <PubSubClient.h>
+ #include <FS.h>                   // Include the SPIFFS library
+ #include <Wire.h>
+ #include "SSD1306Wire.h"
+ #include <Adafruit_Sensor.h>
+ #include <DHT.h>
+ #include <DHT_U.h>
+
+/* ESP12E / NodeMCU default Pin MAP     */
+/* PIN No.  Input Ok    Output Ok
+  n D0
+  a D1         X           X
+  a D2         X           X
+  o D3                     X ( pulled up )
+  o D4                     X ( pulled up )
+  a D5         X           X
+  a D6         X           X
+  a D7         X           X
+  o D8                     X ( pulled low, boot fails if high at boot )
+    TX
+    RX
+*/
 
 /* Build time configuration defines. comment out to disable functionality */
-/* THESE DEFINES ARE NOT CURRENTLY USED                                   */
 #define DEVICE_HAS_EEPROM
 #define DEVICE_HAS_HTTP
 #define DEVICE_HAS_MDNS
@@ -35,37 +69,66 @@
 #define DEVICE_HAS_SPIFFS
 #define DEVICE_HAS_MIN_TICKER
 #define DEVICE_HAS_SEC_TICKER
+#define DEVICE_HAS_SPEAKER
+#define DEVICE_HAS_SD1306_SCREEN
+//#define DEVICE_HAS_DHT_SENSOR
 
-/*  Include all headers needed for full device functionality */
-#include <Arduino.h>
-#include <ESP8266WiFi.h>          // https://github.com/esp8266/Arduino
-#include <DNSServer.h>            // Include for network functionality
-#include <ESP8266WebServer.h>     // Include for web server functionality
-#include <WiFiManager.h>          // https://github.com/tzapu/WiFiManager
-#include <WiFiClient.h>           // for connection to AP
-#include <Ticker.h>               // for LED status indicator
-#include <NTPClient.h>
-#include <WiFiUdp.h>
-#include <ArduinoOTA.h>
-#include <ESP8266mDNS.h>          // Include the mDNS library
-#include <EEPROM.h>               // for storing configuration
-#include <PubSubClient.h>
-#include <FS.h>                   // Include the SPIFFS library
-#include <Wire.h>
+/* HTTP define configurations */
+#ifdef DEVICE_HAS_HTTP
+#define HTTP_SERVER_PORT  80
+ESP8266WebServer server( HTTP_SERVER_PORT );              // web server
+#endif
+
+/* SD1306 Screen define configurations */
+#ifdef DEVICE_HAS_SD1306_SCREEN
+#define SCREEN_WIDTH                128
+#define SCREEN_HEIGHT               64
+#define SCL_PIN             D2
+#define SDA_PIN             D5
+SSD1306Wire  display(0x3c, SDA_PIN, SCL_PIN); // SSD1306 Display init
+#endif
+
+/* Speaker define configurations             */
+#ifdef DEVICE_HAS_SPEAKER
+#define SPEAKER_PIN         D1
+#endif
+
+/* DHT temp & humidity sensor define configurations */
+#ifdef DEVICE_HAS_DHT_SENSOR
+#define DHTPIN              D3    // Digital pin connected to the DHT sensor
+#define DHTTYPE             DHT11     // DHT 11
+//#define DHTTYPE             DHT22     // DHT 22 (AM2302)
+//#define DHTTYPE             DHT21     // DHT 21 (AM2301)
+DHT_Unified dht(DHTPIN, DHTTYPE);
+#endif
+
+/* EEPROM storage define configurations */
+#ifdef DEVICE_HAS_EEPROM
+#define EEPROM_DATA_SIZE    512             // EEPROM data partition size
+#endif
+
+/* NTP define configurations */
+#ifdef DEVICE_HAS_NTP
+//#define NTP_SERVER          "time.nist.gov" // time server
+#define NTP_SERVER          "pool.ntp.org" // time server
+WiFiUDP ntpUDP;                             // NTP UDP object
+NTPClient timeClient(ntpUDP, NTP_SERVER, (-7 * 3600), 3600);  // NTP Client
+#endif
+
+/* MQTT define configurations */
+#ifdef DEVICE_HAS_MQTT
+#define MQTT_SERVER         "greysic.com"   // MQTT server
+const char* mqtt_server = MQTT_SERVER;
+WiFiClient espClient;
+PubSubClient client(espClient);
+#endif
 
 /* some useful defines for all projects */
 #define STATUS_LED          LED_BUILTIN     // on board LED control
 #define BLINKER_FAST        0.2
 #define BLINKER_SLOW        2
 #define BLINKER_NORMAL      0.5
-
-#define EEPROM_DATA_SIZE    512             // EEPROM data partition size
-
 #define MAC_ADDR_LEN        6
-
-#define NTP_SERVER          "time.nist.gov" // time server
-
-#define MQTT_SERVER         "greysic.com"   // MQTT server
 
 /* function prototypes for IOT built-in IOT functionality */
 void run_server_tasks( void );
@@ -81,6 +144,8 @@ int init_eeprom( void );
 int eeprom_read_buf( char *buf, unsigned int offset, unsigned int size );
 int eeprom_write_buf( char *buf, unsigned int offset, unsigned int size );
 int init_spiffs( void );
+int init_display( void );
+void init_dht_sensor( void );
 void init_OTA_upgrade( void );
 void reboot_esp( void );
 void reset_wifi_config( void );
@@ -89,47 +154,54 @@ void mqtt_callback(char* topic, byte* payload, unsigned int length);
 void shutdown_services( void );
 void get_last_reset_reason( void );
 void print_esp8266_data( void );
+void display_temp_humidity( void );
+void display_clock ( void );
 
 /* Global variables for IOT functionality  */
-static unsigned char dev_mac[ MAC_ADDR_LEN ];            // MAC Address of this device
+static unsigned char dev_mac[ MAC_ADDR_LEN ];      // MAC Address of this device
 static char dev_name[ 64 ];                        // device name
 Ticker blink_ticker;                        // defualt LED blinker functionality
 Ticker once_per_sec;                        // second ticker task ( DONT BLOCK );
 Ticker once_per_min;                        // minute ticker task ( DONT BLOCK );
+static unsigned int DeviceConnected = 0;
+static unsigned int update_sec_flag = 1;
+static unsigned int update_min_flag = 1;
 /* wifi Manager variables/objects */
 static WiFiManager wifiManager;             // wifiManager for wifi configuration
 void configModeCallback ( WiFiManager *myWiFiManager );
-/* NTP variables/objects */
-WiFiUDP ntpUDP;                             // NTP UDP object
-NTPClient timeClient(ntpUDP, NTP_SERVER, (-7 * 3600), 3600);  // NTP Client
-/* web server variables/objects */
-ESP8266WebServer server( 80 );              // web server
-/* MQTT variables/objects */
-const char* mqtt_server = MQTT_SERVER;
-WiFiClient espClient;
-PubSubClient client(espClient);
 
 void setup() {
   Serial.begin(115200);
   delay(1000);
   Serial.println("Starting...");
+  init_display();
   get_last_reset_reason();
   print_esp8266_data();
   pinMode( STATUS_LED, OUTPUT );                // set pin for led STATUS
   WiFi.macAddress( ( byte * )dev_mac );         // fill in the devices mac address
   blink_ticker.attach(BLINKER_NORMAL, blinker_task);       // start the default blinker task
+
+#ifdef DEVICE_HAS_SEC_TICKER
   once_per_sec.attach(1, once_per_sec_task);
+#endif
+#ifdef DEVICE_HAS_MIN_TICKER
   once_per_min.attach(60, once_per_min_task);
+#endif
   init_OTA_upgrade();                 // start the over the air upgrade task
   init_eeprom();                      // start the eeprom NV storage
   if( init_spiffs() == 1 ) {          // start the SPIFFS file system
     Serial.printf("spiffs file system formatted\n\r");
   }
+  init_dht_sensor();
 }
 /*     END OF setup()      */
 
 void loop() {
   // put your main code here, to run repeatedly:
+#ifdef DEVICE_HAS_SD1306_SCREEN
+  display.clear();
+#endif
+
   memset( dev_name, 0, sizeof( dev_name ) );
   sprintf( dev_name, "ESP_%02X%02X%02X", dev_mac[3], dev_mac[4], dev_mac[5] );
   Serial.printf( "%s - START, attempting to connect to wifi\n\r", dev_name );
@@ -137,38 +209,54 @@ void loop() {
   if ( !wifiManager.autoConnect( dev_name ) ) {
     Serial.printf( "Failed to connect and hit timeout\n\r" );
   }
-  delay(300);
+  delay(100);
   /* past this line the unit is connected to wifi */
+  DeviceConnected = 1;
   WiFi.hostname( dev_name );
-
   blink_ticker.detach();
   digitalWrite(STATUS_LED, LOW);
   delay(100);
-  timeClient.begin();
-  delay(100);
-  timeClient.update();
 
+#ifdef DEVICE_HAS_HTTP
   // start the web server
   server.on( "/", handle_root );
   server.on("/config", handle_config );
   server.onNotFound( handle_404 );
-
   server.begin();
+#endif
+
   Serial.printf( "SSID: %s\n\r", WiFi.SSID().c_str() );
   Serial.printf( "  IP: %s\n\r", WiFi.localIP().toString().c_str() );
   Serial.printf( "NAME: %s\n\r", WiFi.hostname().c_str() );
 
+#ifdef DEVICE_HAS_SD1306_SCREEN
+  display.clear();
+  display.setFont(ArialMT_Plain_16);
+  display.drawString(5, 0, WiFi.hostname().c_str());
+  display.drawString(5, 18, WiFi.localIP().toString().c_str());
+  display.display();
+#endif
+
+#ifdef DEVICE_HAS_MDNS
   if (!MDNS.begin(dev_name)) {             // Start the mDNS responder for esp8266.local
     Serial.println("Error setting up MDNS responder!");
   } else {
     Serial.println("mDNS responder started");
   }
+#endif
 
+#ifdef DEVICE_HAS_MQTT
   Serial.printf("connecting to mqtt server @ %s\n\r", mqtt_server);
   client.setServer(mqtt_server, 1883);
   client.setCallback(mqtt_callback);
+#endif
 
+#ifdef DEVICE_HAS_NTP
+  timeClient.begin();
+  delay(100);
+  timeClient.update();
   Serial.printf("Current time: %s\n\r", timeClient.getFormattedTime().c_str());
+#endif
 
   // loop forever or until reboot
   while( true ){
@@ -182,12 +270,30 @@ void loop() {
 /* run_server_tasks is meant to run in the main loop, as a sort of very basic task manager */
 void run_server_tasks( void )
 {
-  ArduinoOTA.handle();          // handle OTA upgrade traffic
-  server.handleClient();        // handle web server traffic
-  if (!client.connected()) {    // handle MQTT connections
-    reconnect();
+  if( DeviceConnected == 1 ){
+#ifdef DEVICE_HAS_OTA
+    ArduinoOTA.handle();          // handle OTA upgrade traffic
+#endif
+#ifdef DEVICE_HAS_HTTP
+    server.handleClient();        // handle web server traffic
+#endif
+#ifdef DEVICE_HAS_MQTT
+    if (!client.connected()) {    // handle MQTT connections
+      reconnect();
+    }
+    client.loop();
+#endif
   }
-  client.loop();
+  // offline server tasks below
+  if( update_sec_flag == 1 ) {
+    display_clock();
+    update_sec_flag = 0;
+  }
+  if( update_min_flag == 1 ) {
+    display_temp_humidity();
+    update_min_flag = 0;
+  }
+  yield();
   return ;
 }
 
@@ -204,6 +310,16 @@ void configModeCallback ( WiFiManager *myWiFiManager )
   Serial.println( WiFi.softAPIP() );
   // print the SSID of the config portal
   Serial.println( myWiFiManager->getConfigPortalSSID() );
+
+#ifdef DEVICE_HAS_SD1306_SCREEN
+  display.clear();
+  display.setFont(ArialMT_Plain_16);
+  display.drawString(0, 0, "DISCONNECTED");
+
+  display.drawString(5, 18, myWiFiManager->getConfigPortalSSID());
+  display.setFont(ArialMT_Plain_10);
+  display.drawString(5, 54, WiFi.softAPIP().toString());
+#endif
 }
 
 void blinker_task( void )
@@ -221,10 +337,14 @@ void once_per_sec_task( void )
 {
   /* This task will run once per second, and should not block!
      If read data is needed a flag should be set, and read in the loop, or
-     run_server_tasks functions. 
+     run_server_tasks functions.
   */
   static int second_counter = 0;
+  Serial.printf("sec task %d\n\r", second_counter);
+#ifdef DEVICE_HAS_NTP
   //Serial.printf("sec task %s\n\r", timeClient.getFormattedTime().c_str());
+#endif
+  update_sec_flag = 1;
   second_counter++;
 }
 
@@ -235,23 +355,27 @@ void once_per_min_task ( void )
      run_server_tasks functions.
   */
   static int minute_counter = 0;
-  Serial.printf("min task %s\n\r", timeClient.getFormattedTime().c_str());
+  Serial.printf("min task %d\n\r", minute_counter);
+#ifdef DEVICE_HAS_NTP
+  //Serial.printf("min task %s\n\r", timeClient.getFormattedTime().c_str());
+#endif
+  update_min_flag = 1;
   minute_counter++;
-  if(( minute_counter % 60 ) == 0 ){
-    Serial.printf("[ %3d ] [ %s ] - once_per_minute_task\n\r", minute_counter, timeClient.getFormattedTime().c_str());
-  }
 }
 
 void send_self_redirect( char *msg, int delay )
 {
+#ifdef DEVICE_HAS_HTTP
   char buf[ 256 ];
   memset( buf, 0, sizeof( buf ) );
   sprintf( buf, "<HTML><HEAD><meta http-equiv=\"refresh\" content=\"%i; URL='/'\"/></HEAD><BODY>%s</BODY></HTML>", delay, msg );
   server.send( 200, "text/html", buf);
+#endif
 }
 
 void handle_root( void )
 {
+#ifdef DEVICE_HAS_HTTP
   char buf[ 512 ];
   char mbuf[ 32 ];
   memset( buf, 0, sizeof( buf ) );
@@ -269,11 +393,12 @@ void handle_root( void )
   sprintf( buf+strlen( buf ), "%s", timeClient.getFormattedTime().c_str());
   sprintf( buf+strlen( buf ), "</pre></BODY></HTML>\n\r" );
   server.send( 200, "text/html", buf );
-  return;
+#endif
 }
 
 void handle_404( void )
 {
+#ifdef DEVICE_HAS_HTTP
   String message = "File Not Found\n\n";
   message += "URI: ";
   message += server.uri();
@@ -286,11 +411,12 @@ void handle_404( void )
     message += " " + server.argName( i ) + ": " + server.arg( i ) + "\n";
   }
   server.send( 404, "text/plain", message );
-  return;
+#endif
 }
 
 void handle_config( void )
 {
+#ifdef DEVICE_HAS_HTTP
   char buf[1024];
   int i;
 
@@ -312,50 +438,84 @@ void handle_config( void )
   sprintf( buf+strlen( buf ),"<button onclick=\"document.location='config?wificfg=reset'\">WiFi Config Reset</button><br>\n\r");
   sprintf( buf+strlen( buf ),"</BODY></HTML>\n\r");
   server.send( 200, "text/html", buf );
-  return;
+#endif
 }
 
 int init_eeprom( void )
 {
+#ifdef DEVICE_HAS_EEPROM
   EEPROM.begin(EEPROM_DATA_SIZE);
+#endif
   return 0;
 }
 
 int eeprom_read_buf( char *buf, unsigned int offset, unsigned int size )
 {
-  unsigned int i;
+  unsigned int i = 0;
+#ifdef DEVICE_HAS_EEPROM
   for(i=0;i<size;i++){
     buf[i] = EEPROM.read(offset + i);
   }
+#endif
   return i;
 }
 
 int eeprom_write_buf( char *buf, unsigned int offset, unsigned int size )
 {
-  unsigned int i;
+  unsigned int i = 0;
+#ifdef DEVICE_HAS_EEPROM
   for(i=0;i<size;i++){
     EEPROM.write(offset + i, buf[i]);
   }
+#endif
   return i;
 }
 
 int init_spiffs( void )
 {
   int fs_is_new = 0;
+#ifdef DEVICE_HAS_SPIFFS
   if(!SPIFFS.begin()){
     Serial.printf("formatting file system\n\r");
     SPIFFS.format();
     fs_is_new = 1;
   }
+#endif
   return fs_is_new;
+}
+
+int init_display( void )
+{
+#ifdef DEVICE_HAS_SD1306_SCREEN
+  display.init();
+  display.clear();
+  display.display();
+  display.flipScreenVertically();
+  display.setFont(ArialMT_Plain_16);
+  display.setColor(WHITE);
+  display.setTextAlignment(TEXT_ALIGN_LEFT);
+  //display.setTextAlignment(TEXT_ALIGN_CENTER);
+  display.drawString(10, 20, "boot...");
+  display.display();
+#endif
+  return 0;
+}
+
+void init_dht_sensor( void )
+{
+#ifdef DEVICE_HAS_DHT_SENSOR
+  dht.begin();
+#endif
 }
 
 void init_OTA_upgrade( void )
 {
+#ifdef DEVICE_HAS_OTA
   ArduinoOTA.onStart([]() {
       Serial.println("Upgrade Start");
       blink_ticker.detach();
       blink_ticker.attach(BLINKER_FAST, blinker_task);
+
   });
   ArduinoOTA.onEnd([]() {
       Serial.println("\nUpgrade End...Restarting");
@@ -364,8 +524,20 @@ void init_OTA_upgrade( void )
   });
   ArduinoOTA.onProgress([](unsigned int progress, unsigned int total) {
       Serial.printf("Upgrade Progress: %u%%\r", (progress / (total / 100)));
+#ifdef DEVICE_HAS_SD1306_SCREEN
+      display.clear();
+      display.setFont(ArialMT_Plain_16);
+      display.setColor(WHITE);
+      display.drawString(5, 32, "Upgrade: " + String((int)round((progress / (total / 100)))) + "%");
+      display.display();
+#endif
       if( ( progress / (total / 100)) == 100 ){
           Serial.printf("[ Upgrade Complete ]");
+#ifdef DEVICE_HAS_SD1306_SCREEN
+          display.clear();
+          display.drawString(5, 5, "Complete 100%");
+          display.display();
+#endif
           delay(1000);
       }
   });
@@ -381,6 +553,7 @@ void init_OTA_upgrade( void )
   delay(1000);
   Serial.printf( "WiFi upgrade enabled\n\r");
   ArduinoOTA.begin();
+#endif
 }
 
 void reboot_esp( void )
@@ -399,6 +572,7 @@ void reset_wifi_config( void )
 
 void reconnect()
 {
+#ifdef DEVICE_HAS_MQTT
   String clientId = dev_name;  // clientID = dev_name
   // Loop until we're reconnected
   while (!client.connected()) {
@@ -416,10 +590,12 @@ void reconnect()
       delay(5000);
     }
   }
+#endif
 }
 
 void mqtt_callback(char* topic, byte* payload, unsigned int length)
 {
+#ifdef DEVICE_HAS_MQTT
   unsigned int i;
   Serial.printf("MQTT MSG\n\r");
   Serial.printf("Topic: %s \n\r", topic);
@@ -428,16 +604,25 @@ void mqtt_callback(char* topic, byte* payload, unsigned int length)
     Serial.print((char)payload[i]);
   }
   Serial.println();
+#endif
   return ;
 }
 
 void shutdown_services( void )
 {
   Serial.printf("running shutdown\n\r");
+#ifdef DEVICE_HAS_MIN_TICKER
   once_per_min.detach();
+#endif
+#ifdef DEVICE_HAS_SEC_TICKER
   once_per_sec.detach();
+#endif
+#ifdef DEVICE_HAS_EEPROM
   EEPROM.end();
+#endif
+#ifdef DEVICE_HAS_HTTP
   server.stop();
+#endif
 }
 
 void get_last_reset_reason( void )
@@ -456,4 +641,72 @@ void print_esp8266_data( void )
   Serial.printf("  Prog Use: %u used, %u free\n\r", ESP.getSketchSize(), ESP.getFreeSketchSpace());
   Serial.printf("  Flash ID: %u\n\r", ESP.getFlashChipId());
   Serial.printf("\n\r");
+}
+
+#ifdef DEVICE_HAS_DHT_SENSOR
+  #ifdef DEVICE_HAS_SD1306_SCREEN
+    #define TEMP_DISPLAY_X_OFFSET     75
+    #define TEMP_DISPLAY_Y_OFFSET     54
+    #define HUMIDITY_DISPLAY_X_OFFSET     97
+    #define HUMIDITY_DISPLAY_Y_OFFSET     54
+  #endif
+#endif
+
+void display_temp_humidity( void )
+{
+#ifdef DEVICE_HAS_DHT_SENSOR
+  sensors_event_t event;
+  dht.temperature().getEvent(&event);
+  if (isnan(event.temperature)) {
+    Serial.println(F("Error reading temperature!"));
+  }
+  else {
+    Serial.print(F("Temperature: "));
+    //Serial.print(event.temperature);
+    //Serial.println(F("°C"));
+    Serial.print((int)round((1.8*event.temperature)+32));
+    Serial.println(F("°F"));
+#ifdef DEVICE_HAS_SD1306_SCREEN
+    display.setFont(ArialMT_Plain_10);
+    display.setColor(BLACK);
+    display.fillRect(TEMP_DISPLAY_X_OFFSET, TEMP_DISPLAY_Y_OFFSET, 20, 10);
+    display.setColor(WHITE);
+    display.drawString(TEMP_DISPLAY_X_OFFSET, TEMP_DISPLAY_Y_OFFSET, String((int)round((1.8*event.temperature)+32)) + "°F");
+#endif
+  }
+  // Get humidity event and print its value.
+  dht.humidity().getEvent(&event);
+  if (isnan(event.relative_humidity)) {
+    Serial.println(F("Error reading humidity!"));
+  }
+  else {
+    Serial.print(F("Humidity: "));
+    Serial.print(event.relative_humidity);
+    Serial.println(F("%"));
+#ifdef DEVICE_HAS_SD1306_SCREEN
+    display.setColor(BLACK);
+    display.fillRect(HUMIDITY_DISPLAY_X_OFFSET, HUMIDITY_DISPLAY_Y_OFFSET, 20, 10);
+    display.setColor(WHITE);
+    display.drawString(HUMIDITY_DISPLAY_X_OFFSET, HUMIDITY_DISPLAY_Y_OFFSET, "/ " + String((int)round(event.relative_humidity)) + "%");
+#endif
+  }
+#ifdef DEVICE_HAS_SD1306_SCREEN
+  display.display();
+#endif
+#endif
+}
+
+void display_clock ( void )
+{
+#ifdef DEVICE_HAS_SD1306_SCREEN
+#ifdef DEVICE_HAS_NTP
+  display.setFont(ArialMT_Plain_16);
+  display.setColor(BLACK);
+  display.fillRect(4, 36, 64, 18);
+  display.setColor(WHITE);
+  display.drawString(5, 36, timeClient.getFormattedTime());
+  display.display();
+  update_sec_flag = 0;
+#endif
+#endif
 }
