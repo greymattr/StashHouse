6aed8591af8752583a5b56c48e74d426c6fd06b9
Matthew Fatheree <matthewfatheree@gmail.com>
2020-09-14 09:44:53 -0700

    added more hardware support for IO expanders, changed from spiffs to littleFS, combined i2c devices to one set of pins, prototype code ready for NodeMcu Super ProtoBreadBoard

---
+++ ESP8266_archive/ESP12E_Device_template/README.md
@@ -0,0 +1,5 @@
+<img src="https://github.com/greymattr/StashHouse/blob/master/ESP8266_archive/NodeMCU_mcf_dev_breadboard_bb.png">
+
+# ESP12E NodeMCU dev Board IOT device boiler plate code
+
+> Arduino based code functionality for small IoT base devices based on the NodeMCU hardware platform
--- ESP8266_archive/ESP12E_Device_template/src/main.cpp
+++ ESP8266_archive/ESP12E_Device_template/src/main.cpp
@@ -24,28 +24,31 @@
  * SOFTWARE.
  */
  /*  Include all headers needed for full device functionality */
- #include <Arduino.h>
- #include <ESP8266WiFi.h>          // https://github.com/esp8266/Arduino
- #include <DNSServer.h>            // Include for network functionality
- #include <ESP8266WebServer.h>     // Include for web server functionality
- #include <WiFiManager.h>          // https://github.com/tzapu/WiFiManager
- #include <WiFiClient.h>           // for connection to AP
- #include <Ticker.h>               // for LED status indicator
- #include <NTPClient.h>
- #include <WiFiUdp.h>
- #include <ArduinoOTA.h>
- #include <ESP8266mDNS.h>          // Include the mDNS library
- #include <EEPROM.h>               // for storing configuration
- #include <PubSubClient.h>
- #include <FS.h>                   // Include the SPIFFS library
- #include <Wire.h>
- #include "SSD1306Wire.h"
- #include <Adafruit_Sensor.h>
- #include <DHT.h>
- #include <DHT_U.h>
- #include <HCSR04.h>
- #include "OneButton.h"
-
+#include <Arduino.h>
+#include <ESP8266WiFi.h>          // https://github.com/esp8266/Arduino
+#include <DNSServer.h>            // Include for network functionality
+#include <ESP8266WebServer.h>     // Include for web server functionality
+#include <WiFiManager.h>          // https://github.com/tzapu/WiFiManager
+#include <WiFiClient.h>           // for connection to AP
+#include <Ticker.h>               // for LED status indicator
+#include <NTPClient.h>
+#include <WiFiUdp.h>
+#include <ArduinoOTA.h>
+#include <ESP8266mDNS.h>          // Include the mDNS library
+#include <EEPROM.h>               // for storing configuration
+#include <PubSubClient.h>
+#include <FS.h>
+#include <LittleFS.h>             // Include the LittleFS FIle System library
+#include <Wire.h>
+#include <SSD1306Wire.h>
+#include <Adafruit_Sensor.h>
+#include <DHT.h>
+#include <DHT_U.h>
+#include <HCSR04.h>
+#include <OneButton.h>
+#include <pcf8574_esp.h>
+#include <Wire.h>
+#include <CD74HC4067.h>
 
 /* ESP12E / NodeMCU default Pin MAP     */
 /* PIN No.  Input Ok    Output Ok
@@ -70,15 +73,21 @@
 #define DEVICE_HAS_MQTT
 #define DEVICE_HAS_NTP
 #define DEVICE_HAS_OTA
-#define DEVICE_HAS_SPIFFS
+#define DEVICE_HAS_FILESYSTEM
 #define DEVICE_HAS_MIN_TICKER
 #define DEVICE_HAS_SEC_TICKER
+#define I2C_BUS_SCAN_ENABLED
 /* Add on sensor and IO support section */
 //#define DEVICE_HAS_SPEAKER
-//#define DEVICE_HAS_SD1306_SCREEN
+#define DEVICE_HAS_SD1306_SCREEN
 //#define DEVICE_HAS_DHT_SENSOR
 //#define DEVICE_HAS_HC_SR04
-//#define DEVICE_HAS_BUTTON
+#define DEVICE_HAS_BUTTON
+#define DEVICE_HAS_PCF8574_DIGITAL_IO
+#define DEVICE_HAS_CD74HC4067_ANALOG_IO
+
+#define I2C_SCL_PIN             D4
+#define I2C_SDA_PIN             D3
 
 /* HTTP define configurations */
 #ifdef DEVICE_HAS_HTTP
@@ -90,9 +99,10 @@ ESP8266WebServer server( HTTP_SERVER_PORT );              // web server
 #ifdef DEVICE_HAS_SD1306_SCREEN
 #define SCREEN_WIDTH        128
 #define SCREEN_HEIGH        64
-#define SCL_PIN             D4
-#define SDA_PIN             D3
-SSD1306Wire  display(0x3c, SDA_PIN, SCL_PIN); // SSD1306 Display init
+#define SD1306_ADDR         0x3c
+#define SD1306_SCL_PIN      I2C_SCL_PIN
+#define SD1306_SDA_PIN      I2C_SDA_PIN
+SSD1306Wire  display(SD1306_ADDR, I2C_SDA_PIN, I2C_SCL_PIN); // SSD1306 Display init
 #endif
 
 /* Speaker define configurations             */
@@ -148,6 +158,27 @@ PubSubClient client(espClient);
 OneButton button_1(BUTTON_PIN, false, false);    // active high ( need 4.7K resistor from pin to GND )
 #endif
 
+#ifdef DEVICE_HAS_PCF8574_DIGITAL_IO
+#define PCF8574_SDA_PIN    I2C_SDA_PIN
+#define PCF8574_SCL_PIN    I2C_SCL_PIN
+#define PCF8574_ADDR       0x20
+TwoWire testWire;
+// Initialize a  at I2C-address 0x20, using GPIO5, GPIO4 and testWire for the I2C-bus
+PCF857x pcf8574(PCF8574_ADDR, &testWire);
+#endif
+
+#ifdef DEVICE_HAS_CD74HC4067_ANALOG_IO
+#define CD74HC4067_S0_PIN   D5
+#define CD74HC4067_S1_PIN   D6
+#define CD74HC4067_S2_PIN   D7
+#define CD74HC4067_S3_PIN   D8
+#define CD74HC4067_SIG_PIN  A0
+#define CD74HC406_CHANNEL_COUNT 16
+CD74HC4067 analog_mux(CD74HC4067_S0_PIN, CD74HC4067_S1_PIN, CD74HC4067_S2_PIN, CD74HC4067_S3_PIN);  // create a new CD74HC4067 object with its four control pins
+const int analog_sig_pin = CD74HC4067_SIG_PIN; // select a pin to share with the 16 channels of the CD74HC4067
+static int CD74HC4067_channel_val_array[ CD74HC406_CHANNEL_COUNT ];
+#endif
+
 /* some useful defines for all projects */
 #define STATUS_LED          LED_BUILTIN     // ESP12E on board LED ( active HIGH )
 #define DEV_BOARD_LED       D0              // NodeMCU dev on board LED ( active LOW )
@@ -166,12 +197,12 @@ void send_self_redirect( char *msg, int delay );
 void handle_root( void );
 void handle_404( void );
 void handle_config( void );
-int init_eeprom( void );
+int eeprom_init( void );
 int eeprom_read_buf( char *buf, unsigned int offset, unsigned int size );
 int eeprom_write_buf( char *buf, unsigned int offset, unsigned int size );
-int init_spiffs( void );
-int init_display( void );
-void init_dht_sensor( void );
+int FS_init( void );
+int display_init( void );
+void dht_init( void );
 void init_OTA_upgrade( void );
 void reboot_esp( void );
 void reset_wifi_config( void );
@@ -192,6 +223,19 @@ void longClick();
 void doubleClick();
 void tripleClick();
 void tap();
+void pcf8574_init( void );
+void cd74hc4067_init( void );
+void cd74hc4067_get_array_values( void );
+void cd74hc4067_print_array_values( void );
+void i2c_bus_scan( void );
+void buttons_init( void );
+void listDir(const char * dirname);
+void readFile(const char * path);
+void appendFile(const char * path, const char * message);
+void renameFile(const char * path1, const char * path2);
+void deleteFile(const char * path);
+void tickers_init( void );
+
 
 /* Global variables for IOT functionality  */
 static unsigned char dev_mac[ MAC_ADDR_LEN ];      // MAC Address of this device
@@ -206,51 +250,41 @@ static unsigned int update_min_flag = 1;
 static WiFiManager wifiManager;             // wifiManager for wifi configuration
 void configModeCallback ( WiFiManager *myWiFiManager );
 
-
-int motor_ina = D6;
-int motor_inb = D7;
+static unsigned int second_counter = 0;
+static unsigned int minute_counter = 0;
 
 void setup() {
   Serial.begin(115200);
   delay(1000);
   Serial.println("Starting...");
-  init_display();
-  delay(500);
   get_last_reset_reason();
   print_esp8266_data();
-  pinMode( STATUS_LED, OUTPUT );                   // set pin for STATUS
-  pinMode( DEV_BOARD_LED, OUTPUT );                // set pin for board STATUS
-  WiFi.macAddress( ( byte * )dev_mac );         // fill in the devices mac address
-  blink_ticker.attach(BLINKER_NORMAL, blinker_task);       // start the default blinker task
+  pinMode( STATUS_LED, OUTPUT );                  // set pin for STATUS
+  pinMode( DEV_BOARD_LED, OUTPUT );               // set pin for board STATUS
+  WiFi.macAddress( ( byte * )dev_mac );           // fill in the devices mac address
 
-#ifdef DEVICE_HAS_SEC_TICKER
-  once_per_sec.attach(1, once_per_sec_task);
-#endif
-#ifdef DEVICE_HAS_MIN_TICKER
-  once_per_min.attach(60, once_per_min_task);
-#endif
-
-#ifdef DEVICE_HAS_BUTTON
-  button_1.attachClick(click);
-  button_1.attachDoubleClick(doubleClick);
-#endif
-
-  init_OTA_upgrade();                 // start the over the air upgrade task
-  init_eeprom();                      // start the eeprom NV storage
-  if( init_spiffs() == 1 ) {          // start the SPIFFS file system
-    Serial.printf("spiffs file system formatted\n\r");
+  /*  Internal software based service init(s) */
+  blink_ticker.attach(BLINKER_NORMAL, blinker_task);      // start the default blinker task
+  tickers_init();                        // start minute and second tickers
+  eeprom_init();                         // start the eeprom NV storage
+  if( FS_init() == 1 ) {                 // start the file system
+    Serial.printf("file system formatted\n\r");
   }
-  init_dht_sensor();
+  init_OTA_upgrade();                    // start the over the air upgrade task
+  /* External harware Init(s) */
+  i2c_bus_scan();                        // scan the I2C bus for devices
+  buttons_init();                        // Initialize any buttons
+  display_init();                        // start the display
+  pcf8574_init();                        // start expanded digital IO
+  cd74hc4067_init();                     // start expanded analog INPUT
+  dht_init();                     // start temp and humidity sensor
+  delay(500);
 }
 /*     END OF setup()      */
 
 void loop() {
+
   // put your main code here, to run repeatedly:
-#ifdef DEVICE_HAS_SD1306_SCREEN
-  delay(250);
-  display.clear();
-  display.display();
-#endif
 
   memset( dev_name, 0, sizeof( dev_name ) );
   sprintf( dev_name, "ESP_%02X%02X%02X", dev_mac[3], dev_mac[4], dev_mac[5] );
@@ -267,6 +301,10 @@ void loop() {
   digitalWrite(STATUS_LED, LOW);
   delay(100);
 
+  Serial.printf( "SSID: %s\n\r", WiFi.SSID().c_str() );
+  Serial.printf( "  IP: %s\n\r", WiFi.localIP().toString().c_str() );
+  Serial.printf( "NAME: %s\n\r", WiFi.hostname().c_str() );
+
 #ifdef DEVICE_HAS_HTTP
   // start the web server
   server.on( "/", handle_root );
@@ -274,10 +312,6 @@ void loop() {
   server.onNotFound( handle_404 );
   server.begin();
 #endif
-
-  Serial.printf( "SSID: %s\n\r", WiFi.SSID().c_str() );
-  Serial.printf( "  IP: %s\n\r", WiFi.localIP().toString().c_str() );
-  Serial.printf( "NAME: %s\n\r", WiFi.hostname().c_str() );
 
 #ifdef DEVICE_HAS_SD1306_SCREEN
   display.clear();
@@ -320,6 +354,8 @@ void loop() {
 /* run_server_tasks is meant to run in the main loop, as a sort of very basic task manager */
 void run_server_tasks( void )
 {
+  char ts_buf[128];
+
   if( DeviceConnected == 1 ){
 #ifdef DEVICE_HAS_OTA
     ArduinoOTA.handle();          // handle OTA upgrade traffic
@@ -345,8 +381,10 @@ void run_server_tasks( void )
   if( update_min_flag == 1 ) {
     display_temp_humidity();
     update_min_flag = 0;
+    memset(ts_buf, 0, sizeof(ts_buf));
+    sprintf(ts_buf, "%s - ( %d mins upTime )\n\r",timeClient.getFormattedTime().c_str(), minute_counter );
+    client.publish("/esp9vUptime", ts_buf);
   }
-  //yield();
   return ;
 }
 
@@ -394,24 +432,11 @@ void once_per_sec_task( void )
      If read data is needed a flag should be set, and read in the loop, or
      run_server_tasks functions.
   */
-  static int second_counter = 0;
-  //Serial.printf("sec task %d\n\r", second_counter);
-#ifdef DEVICE_HAS_NTP
-  //Serial.printf("sec task %s\n\r", timeClient.getFormattedTime().c_str());
+#if 0
+  Serial.printf("sec task %d\n\r", second_counter);
 #endif
   update_sec_flag = 1;
   second_counter++;
-
-#if 0
-  if( second_counter < 50 ){
-    Serial.printf("motor speed %d\n\r", second_counter * 10);
-    analogWrite(motor_ina, ( second_counter * 10 ));
-    digitalWrite(motor_inb, LOW);
-  } else {
-    analogWrite(motor_ina, 160);
-    digitalWrite(motor_inb, LOW);
-  }
-#endif
 }
 
 void once_per_min_task ( void )
@@ -420,10 +445,8 @@ void once_per_min_task ( void )
      If read data is needed a flag should be set, and read in the loop, or
      run_server_tasks functions.
   */
-  static int minute_counter = 0;
-  Serial.printf("min task %d\n\r", minute_counter);
 #ifdef DEVICE_HAS_NTP
-  //Serial.printf("min task %s\n\r", timeClient.getFormattedTime().c_str());
+  Serial.printf("min task %s ( %d )\n\r", timeClient.getFormattedTime().c_str(), minute_counter);
 #endif
   update_min_flag = 1;
   minute_counter++;
@@ -507,7 +530,7 @@ void handle_config( void )
 #endif
 }
 
-int init_eeprom( void )
+int eeprom_init( void )
 {
 #ifdef DEVICE_HAS_EEPROM
   EEPROM.begin(EEPROM_DATA_SIZE);
@@ -537,20 +560,25 @@ int eeprom_write_buf( char *buf, unsigned int offset, unsigned int size )
   return i;
 }
 
-int init_spiffs( void )
+int FS_init( void )
 {
   int fs_is_new = 0;
-#ifdef DEVICE_HAS_SPIFFS
-  if(!SPIFFS.begin()){
+#ifdef DEVICE_HAS_FILESYSTEM
+  Serial.println("Mount LittleFS");
+  if (!LittleFS.begin()) {
     Serial.printf("formatting file system\n\r");
-    SPIFFS.format();
+    LittleFS.format();
+    if (!LittleFS.begin()) {
+      Serial.println("LittleFS mount failed");
+      fs_is_new = -1;
+    }
     fs_is_new = 1;
   }
 #endif
   return fs_is_new;
 }
 
-int init_display( void )
+int display_init( void )
 {
 #ifdef DEVICE_HAS_SD1306_SCREEN
   delay(100);
@@ -565,11 +593,14 @@ int init_display( void )
   //display.setTextAlignment(TEXT_ALIGN_CENTER);
   display.drawString(10, 20, "screen init");
   display.display();
+  delay(1000);
+  display.clear();
+  display.display();
 #endif
   return 0;
 }
 
-void init_dht_sensor( void )
+void dht_init( void )
 {
 #ifdef DEVICE_HAS_DHT_SENSOR
   dht.begin();
@@ -614,6 +645,12 @@ void init_OTA_upgrade( void )
           display.display();
 #endif
           delay(1000);
+          display.clear();
+          display.drawString(5, 5, "Reboot");
+          display.display();
+          delay(500);
+          display.clear();
+          display.display();
       }
   });
   ArduinoOTA.onError([](ota_error_t error) {
@@ -728,6 +765,7 @@ void print_esp8266_data( void )
   Serial.printf("  Prog Use: %u used, %u free\n\r", ESP.getSketchSize(), ESP.getFreeSketchSpace());
   Serial.printf("  Flash ID: %u\n\r", ESP.getFlashChipId());
   Serial.printf(" Board LED: %d\n\r", STATUS_LED);
+#if 0
   Serial.printf("   Pin Map:\n\r");
   Serial.printf("    %d = D0\n\r", D0);
   Serial.printf("    %d = D1\n\r", D1);
@@ -739,7 +777,9 @@ void print_esp8266_data( void )
   Serial.printf("    %d = D7\n\r", D7);
   Serial.printf("    %d = D8\n\r", D8);
   Serial.printf("    %d = A0\n\r", A0);
+#endif
   Serial.printf("\n\r");
+
 }
 
 void display_temp_humidity( void )
@@ -836,9 +876,6 @@ void changed() {
 }
 void click() {
     Serial.println("click\n");
-#ifdef DEVICE_HAS_SPEAKER
-    tone(SPEAKER_PIN, 1400, 500);
-#endif
 }
 void longClick() {
     Serial.println("long click\n");
@@ -851,4 +888,195 @@ void tripleClick() {
 }
 void tap() {
     Serial.println("tap");
+}
+
+void pcf8574_init( void )
+{
+#ifdef DEVICE_HAS_PCF8574_DIGITAL_IO
+  testWire.begin(PCF8574_SDA_PIN, PCF8574_SCL_PIN);
+  //Specsheets say  is officially rated only for 100KHz I2C-bus
+  //PCF8575 is rated for 400KHz
+  //testWire.setClock(100000L);
+  pcf8574.begin();
+#endif
+}
+
+void cd74hc4067_init( void )
+{
+#ifdef DEVICE_HAS_CD74HC4067_ANALOG_IO
+  // select pin using analog_mux.channel(i);
+  // read using analogRead(analog_sig_pin) ( READ ONLY Analog );
+  pinMode(analog_sig_pin, INPUT);
+#endif
+}
+
+void cd74hc4067_get_array_values( void )
+{
+#ifdef DEVICE_HAS_CD74HC4067_ANALOG_IO
+  unsigned int i;
+  for(i = 0;i<CD74HC406_CHANNEL_COUNT;i++){
+    analog_mux.channel(i);
+    CD74HC4067_channel_val_array[i] = analogRead(analog_sig_pin);
+  }
+#endif
+}
+
+void cd74hc4067_print_array_values( void )
+{
+#ifdef DEVICE_HAS_CD74HC4067_ANALOG_IO
+  unsigned int i;
+  cd74hc4067_get_array_values( );
+
+  for(i = 0;i<CD74HC406_CHANNEL_COUNT;i++){
+    Serial.printf("C%02d: %04d\n\r", i, CD74HC4067_channel_val_array[i]);
+  }
+#endif
+}
+
+void i2c_bus_scan( void )
+{
+#ifdef I2C_BUS_SCAN_ENABLED
+  unsigned int count = 0;
+  Serial.printf("I2C Devices:\n\r");
+  Wire.begin(I2C_SDA_PIN,I2C_SCL_PIN);
+  for (byte i = 8; i < 120; i++)
+  {
+    Wire.beginTransmission(i);
+    if (Wire.endTransmission() == 0) {
+      Serial.print("Found I2C Device: ");
+      Serial.print(" (0x");
+      Serial.print(i, HEX);
+      Serial.println(")");
+      count++;
+      delay(1);
+    }
+  }
+#endif
+}
+
+void buttons_init( void )
+{
+#ifdef DEVICE_HAS_BUTTON
+  button_1.attachClick(click);
+  button_1.attachDoubleClick(doubleClick);
+#endif
+}
+
+
+void listDir(const char * dirname)
+{
+#ifdef DEVICE_HAS_FILESYSTEM
+  Serial.printf("Listing directory: %s\n", dirname);
+
+  Dir root = LittleFS.openDir(dirname);
+
+  while (root.next())
+  {
+    File file = root.openFile("r");
+    Serial.print("  FILE: ");
+    Serial.print(root.fileName());
+    Serial.print("  SIZE: ");
+    Serial.print(file.size());
+    time_t cr = file.getCreationTime();
+    time_t lw = file.getLastWrite();
+    file.close();
+    struct tm * tmstruct = localtime(&cr);
+    Serial.printf("    CREATION: %d-%02d-%02d %02d:%02d:%02d\n", (tmstruct->tm_year) + 1900, (tmstruct->tm_mon) + 1, tmstruct->tm_mday, tmstruct->tm_hour, tmstruct->tm_min, tmstruct->tm_sec);
+    tmstruct = localtime(&lw);
+    Serial.printf("  LAST WRITE: %d-%02d-%02d %02d:%02d:%02d\n", (tmstruct->tm_year) + 1900, (tmstruct->tm_mon) + 1, tmstruct->tm_mday, tmstruct->tm_hour, tmstruct->tm_min, tmstruct->tm_sec);
+  }
+#endif
+}
+
+
+void readFile(const char * path)
+{
+#ifdef DEVICE_HAS_FILESYSTEM
+  Serial.printf("Reading file: %s\n", path);
+
+  File file = LittleFS.open(path, "r");
+  if (!file) {
+    Serial.println("Failed to open file for reading");
+    return;
+  }
+
+  Serial.print("Read from file: ");
+  while (file.available()) {
+    Serial.write(file.read());
+  }
+  file.close();
+#endif
+}
+
+void writeFile(const char * path, const char * message)
+{
+#ifdef DEVICE_HAS_FILESYSTEM
+  Serial.printf("Writing file: %s\n", path);
+
+  File file = LittleFS.open(path, "w");
+  if (!file) {
+    Serial.println("Failed to open file for writing");
+    return;
+  }
+  if (file.print(message)) {
+    Serial.println("File written");
+  } else {
+    Serial.println("Write failed");
+  }
+  delay(2000); // Make sure the CREATE and LASTWRITE times are different
+  file.close();
+#endif
+}
+
+void appendFile(const char * path, const char * message)
+{
+#ifdef DEVICE_HAS_FILESYSTEM
+  Serial.printf("Appending to file: %s\n", path);
+
+  File file = LittleFS.open(path, "a");
+  if (!file) {
+    Serial.println("Failed to open file for appending");
+    return;
+  }
+  if (file.print(message)) {
+    Serial.println("Message appended");
+  } else {
+    Serial.println("Append failed");
+  }
+  file.close();
+#endif
+}
+
+void renameFile(const char * path1, const char * path2)
+{
+#ifdef DEVICE_HAS_FILESYSTEM
+  Serial.printf("Renaming file %s to %s\n", path1, path2);
+  if (LittleFS.rename(path1, path2)) {
+    Serial.println("File renamed");
+  } else {
+    Serial.println("Rename failed");
+  }
+#endif
+}
+
+void deleteFile(const char * path)
+{
+#ifdef DEVICE_HAS_FILESYSTEM
+  Serial.printf("Deleting file: %s\n", path);
+  if (LittleFS.remove(path)) {
+    Serial.println("File deleted");
+  } else {
+    Serial.println("Delete failed");
+  }
+#endif
+}
+
+void tickers_init( void )
+{
+  #ifdef DEVICE_HAS_SEC_TICKER
+    once_per_sec.attach(1, once_per_sec_task);
+  #endif
+  #ifdef DEVICE_HAS_MIN_TICKER
+    once_per_min.attach(60, once_per_min_task);
+  #endif
 }
