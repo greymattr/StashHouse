f268d4f2a0a3801ba96b2c47f4d1c7a4afbb3934
Matthew Fatheree <matthewfatheree@gmail.com>
2021-06-07 09:42:00 -0700

    update .tcshrc script and add script to install default utils on linux

---
+++ utils/apt-install_greyutils.sh
@@ -0,0 +1,4 @@
+#!/bin/sh
+#script to use apt to install tools I commonly use on linux
+
+sudo apt-get -y install vim-gtk screen terminator tcsh build-essential yakuake lxc squashfs-tools wireshark iperf tcpdump xtightvncviewer xvnc4viewer vlc minicom dos2unix elinks espeak ethtool ffmpeg ncftp libwidevinecdm0 hunt vpnc openconnect
