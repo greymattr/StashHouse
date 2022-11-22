7f1e9c21b3d3ee90812195abbc775e9840478d24
Matthew Fatheree <matthewfatheree@gmail.com>
2022-11-22 11:13:19 -0800

    update vim template files

---
+++ utils/vim_templates/source_basic.c
@@ -0,0 +1,21 @@
+/* this is a basic c template file */
+#include <stdio.h>
+#include <stdlib.h>
+#include <strings.h>
+
+int main( int argc, char **argv )
+{
+
+
+  return 0;
+}
+
+/*
+	void print_use( void ) 
+	{
+	
+	return;
+	}
+*/
+
+
