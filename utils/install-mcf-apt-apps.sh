541ce2b5002a927ae95c17e5209fd69ceee81996
Matthew Fatheree <greymattr@gmail.com>
2024-02-08 07:42:29 -0800

    add script to install apps I use on Linux

---
+++ utils/install-mcf-apt-apps.sh
@@ -0,0 +1,50 @@
+#!/usr/bin/env bash
+
+apt install -y \
+	gawk \
+	wget \
+	diffstat \
+	unzip \
+	gcc \
+	build-essential \
+	chrpath \
+	socat \
+	cpio \
+	python3 \
+	python3-git \
+	python3-pip \
+	pylint \
+	xz-utils \
+	debianutils \
+	iputils-ping \
+	libegl1-mesa \
+	libsdl1.2-dev \
+	mesa-common-dev \
+	xterm \
+	zstd \
+	bmap-tools \
+	htop \
+	screen \
+	tmux \
+	vim \
+  chrpath \
+  gawk \
+  lz4 \
+  gh \
+  python3-distutils \
+  terminator \
+  minicom \
+  vim \
+  ranger \
+  wireshark \
+  tcpdump \
+  asciinema \
+  git \
+  diffuse \
+  wget \
+  curl \
+  tcsh \
+  openssh-server \
+  nmap \
+  netcat
+  
